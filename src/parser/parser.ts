e0db67aee77d7d17d03dbd8b6d5ab1192b69df70
Samantha Quiñones <samantha@ieatkillerbe.es>
2023-01-22 19:54:17 +0000

    basic language detection

--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -1,6 +1,25 @@
-export default class Parser {
 
-    parse (input: string): string {
-        return "Hello world";
+import { Language } from "node-nlp";
+
+export const SUPPORTED_LANGUAGES = [ "en", "es", "de", "fi", "sv"];
+
+export type ParseResult = {
+    detectedLanguage: DetectedLanguage
+}
+
+export type DetectedLanguage = {
+    "alpha2": string,
+    "alpha3": string,
+    "language": string,
+    "score": number,
+}
+
+export default class Parser {
+    private language = new Language();
+
+    parse (input: string): ParseResult {
+        return {
+            detectedLanguage: this.language.guessBest(input, SUPPORTED_LANGUAGES),
+        }
     }
 }
--- test/parser/parser.test.ts
+++ test/parser/parser.test.ts
@@ -1,6 +1,18 @@
 import Parser from "../../src/parser";
 
-test('Parser returns hello world', () => {
+test('Case 1 - English text, no intents', () => {
+    const expected = {
+        detectedLanguage: {"alpha2": "en", "alpha3": "eng", "language": "English","score": 1} 
+    };
+
     const parser = new Parser();
-    expect(parser.parse("foo bar")).toBe("Hello world");
+    expect(parser.parse("hello world I love you")).toEqual(expected);
+})
+
+test('Case 2 - Spanish text, no intents', () => {
+    const expected = { 
+        detectedLanguage: {"alpha2": "es", "alpha3": "spa", "language": "Spanish","score": 1} 
+    }
+    const parser = new Parser();
+    expect(parser.parse("hola al mundo")).toEqual(expected);
 })
